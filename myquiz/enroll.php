89623a99613ebea14e48a9dfe444fe17e2d643f7
Eneko Ortiz de Zarate <eortizdezarate001@ikasle.ehu.eus>
2016-09-29 10:28:06 +0200

    Datubasea hostinger-an sortu eta gero.

--- myquiz/enroll.php
+++ myquiz/enroll.php
@@ -1,5 +1,6 @@
 <?php
-  $connect = mysqli_connect("localhost","root","", "quiz");
+  //$connect = mysqli_connect("localhost","root","", "quiz");
+  $connect = mysqli_connect("mysql.hostinger.es","u102514866_eneko","eortizdezarate001","u102514866_quiz");
   $sql = "INSERT INTO Erabiltzailea
           VALUES ('$_POST[firstname]','$_POST[lastname]','$_POST[email]','$_POST[password]','$_POST[phone]','$_POST[specialty]','$_POST[interests]')";
   $query = mysqli_query($connect,$sql);
